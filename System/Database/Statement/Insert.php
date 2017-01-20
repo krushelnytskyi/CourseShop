8189945c984fed4bcae0059f12c68a0e08baf479
Igor Kril <vovkodav84@gmail.com>
2017-01-20 12:09:27 +0200

    Insert class

--- System/Database/Statement/Insert.php
+++ System/Database/Statement/Insert.php
@@ -10,24 +10,57 @@ class Insert extends Statement
     /**
      * @var array
      */
-    private $values = [];
+    protected $values = array();
 
     /**
-     * @param $values
-     * @return $this
+     * @var array
      */
-    public function values($values)
+    protected $variables = array();
+
+
+    /**
+     * @param   array   $values  values list
+     * @param   ...
+     * @return  $this
+     */
+    public function values(array $values)
     {
-        $this->values = $values;
+        if ( ! is_array($this->values))
+        {
+            throw new Exception('INSERT INTO ... SELECT statements cannot be combined with INSERT INTO ... VALUES');
+        }
+        $this->variables = array_values($values);
+        $this->values = array_values($values);
+
         return $this;
     }
 
+
     /**
-     *
+     * @param array $variables
+     * @return bool
      */
+
     public function execute()
     {
+        $sql = "INSERT INTO " . $this->table;
+        $fields = array();
+        $values = array();
+        foreach ($this->variables as $field => $value) {
+            $fields[] = $field;
+            $values[] = "'" . $value . "'";
+        }
+        $fields = ' (' . implode(', ', $fields) . ')';
+        $values = '(' . implode(', ', $values) . ')';
 
+        $sql .= $fields . ' VALUES ' . $values;
+
+        $result = $this->connection->getLink()->query($sql);
+
+        return (false === $result) ? false : $this->connection->getLink()->insert_id;
     }
-
 }
+
+
+
+
